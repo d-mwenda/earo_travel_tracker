f19cfab2a8b8758862b0b60e030d4a65f4addc29
Derick Mwenda <52851761+d-mwenda@users.noreply.github.com>
2020-09-28 13:07:17 +0300

    update gitignore

--- backend/earo_travel_tracker/trip/tests.py
+++
@@ -1,7 +0,0 @@
-"""
-Tests for the trip app.
-"""
-from django.test import TestCase
-
-# Create your tests here.
-# Todo test that new trips are unapproved by default and modifications to a trip instance do not affect approval.
