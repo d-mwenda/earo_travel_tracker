a31ebb46dfa4250b2c6071f2316383ce0172fbe4
Derick Mwenda <52851761+d-mwenda@users.noreply.github.com>
2021-01-13 15:11:04 +0300

    Automatically assign a user permissions to change their profile when the user logs into the system for the first time. Add a user to the travelers group so that they get default traveler permissions.

--- backend/earo_travel_tracker/traveler/signals.py
+++ backend/earo_travel_tracker/traveler/signals.py
@@ -4,27 +4,41 @@ One important signal will be for creating a Traveler profile every time a
 new User instance is created.
 Many signals will be post-save to assign various permissions.
 """
+import logging
 # django imports
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+from django.contrib.auth.models import Group
 # third-party app imports
 from guardian.shortcuts import assign_perm
 # earo_travel_tracker imports
 from traveler.models import TravelerProfile
 
+logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_traveler_profile(sender, **kwargs):
     """
     Create traveler profile for and whenever every User instance created.
+    Assign the user all the basic permissions for a traveler by adding them
+    to traveler group.
     """
-    instance = kwargs['instance']
+    user = kwargs['instance']
     if kwargs['created']:
-        TravelerProfile(
-                        type_of_traveler='Employee',
-                        nationality='Kenyan',
-                        contact_telephone='',
-                        contact_email=instance.email,
-                        user_account=instance,
-                    ).save()
+        profile = TravelerProfile(
+                    type_of_traveler='Employee',
+                    nationality='Kenyan',
+                    contact_telephone='',
+                    contact_email=user.email,
+                    user_account=user,
+        ).save()
+        logger.debug("Created profile for %s", user.username)
+        assign_perm("traveler.change_travelerprofile", user, profile)
+        logger.debug("Granted %s permission to change own profile", user.username)
+        try:
+            travelers = Group.objects.get(name='travelers')
+            travelers.user_set.add(user)
+        except Group.DoesNotExist:
+            logger.error("Travelers group doesn't exist." \
+                "Create the group and assign it the proper permissions")
